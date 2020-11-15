4996d40cd6449fb05c08ea0046fe5ef3c58ca5d5
waincent <wangbin@gmail.com>
2020-11-15 22:12:25 +0800

    project init

---
+++ app/src/main/java/com/waincent/wisdom/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.waincent.wisdom
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+//import androidx.compose.foundation.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.ui.tooling.preview.Preview
+import com.waincent.wisdom.ui.WisdomTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WisdomTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    WisdomTheme {
+        Greeting("Android")
+    }
+}
